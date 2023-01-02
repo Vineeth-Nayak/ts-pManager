02b14bbf105fb1c71f0f75920e7fa8fa6872b44f
Vineeth-Nayak <vineeth.pnayak@robosoft.com>
2023-01-02 12:36:56 +0530

    commit initial structure

---
+++ config/default.ts
@@ -0,0 +1,5 @@
+export default {
+  port: 5000,
+  dbUri: "mongodb://127.0.0.1/ts-password-manager",
+  logLevel: "info",
+};
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+require("dotenv").config();
+import express, { Request, Response } from "express";
+import config from "config";
+import connectToDb from "./utils/connectToDb";
+import log from "./utils/logger";
+
+const port = process.env.PORT || config.get("port") || 5000;
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  return res.send("Hello World");
+});
+
+app.listen(port, () => {
+  //   console.log("Running on port", port);
+  log.info(`Running on port ${port}`);
+  connectToDb();
+});
